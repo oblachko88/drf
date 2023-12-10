cef21b527c637b3730f5ab11865a1402e5434dad
oblako88 <88oblachko@gmail.com>
2023-12-10 13:52:28 +0200

    commit

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,26 +1,41 @@
 import json
 from django.http import JsonResponse
 
+from products.models import Product
+
 
 def api_home(request, *args, **kwargs):
-    # request -> HttpRequest -> Django
-    # request.body
-    # print(dir(request))
-    print(request.GET) # url query params 
-    print(request.POST)
+    model_data = Product.objects.all().order_by("?").first()
+    # This order_by("?") means we take only one random object from all of them. 
+    data = {} 
 
-    body = request.body # byte string of JSON Data
-    data = {}
-        
-    try:
-        data = json.loads(body) # string of JSON -> Python Dict
-    except:
-        pass
-
-    print(data)
-    
-    data['params'] = dict(request.GET)
-    data['headers'] = dict(request.headers)
-    data['content'] = request.content_type
-    
+    if model_data:
+        data['id'] = model_data.id
+        data['title'] = model_data.title
+        data['content'] = model_data.content
+        data['price'] = model_data.price
     return JsonResponse(data)
+
+# def api_home(request, *args, **kwargs):
+#     # request -> HttpRequest -> Django
+#     # request.body
+#     # print(dir(request))
+
+#     print(request.GET) # url query params 
+#     print(request.POST)
+
+#     body = request.body # byte string of JSON Data
+#     data = {}
+        
+#     try:
+#         data = json.loads(body) # string of JSON -> Python Dict
+#     except:
+#         pass
+
+#     print(data)
+    
+#     data['params'] = dict(request.GET)
+#     data['headers'] = dict(request.headers)
+#     data['content'] = request.content_type
+
+#     return JsonResponse(data)
