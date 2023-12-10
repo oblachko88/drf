b1f4022bcb5d6bc8c0a5e22d2b703a00bfd51881
oblako88 <88oblachko@gmail.com>
2023-12-10 12:20:12 +0200

    commit

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -7,3 +7,5 @@ urlpatterns = [
     path('', views.api_home) # So here we can just use the api_home function that we could import from .views
     # This also would be the localhost:8000/api/
 ]
+
+
--- backend/api/views.py
+++ backend/api/views.py
@@ -6,14 +6,21 @@ def api_home(request, *args, **kwargs):
     # request -> HttpRequest -> Django
     # request.body
     # print(dir(request))
+    print(request.GET) # url query params 
+    print(request.POST)
 
     body = request.body # byte string of JSON Data
     data = {}
-
+        
     try:
         data = json.loads(body) # string of JSON -> Python Dict
     except:
         pass
 
     print(data)
+    
+    data['params'] = dict(request.GET)
+    data['headers'] = dict(request.headers)
+    data['content'] = request.content_type
+    
     return JsonResponse(data)
