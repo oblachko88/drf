735263c8c6c8abdc1afba3717ed692c2737c71d4
oblako88 <88oblachko@gmail.com>
2023-12-10 14:05:32 +0200

    commit

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,10 @@
 import json
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
+from django.forms.models import model_to_dict
+    # SERIALIZATION
+    # model instance (model_data)
+    # turn a Python dict 
+    # return JSON to my client
 
 from products.models import Product
 
@@ -7,13 +12,11 @@ from products.models import Product
 def api_home(request, *args, **kwargs):
     model_data = Product.objects.all().order_by("?").first()
     # This order_by("?") means we take only one random object from all of them. 
-    data = {} 
+    data = {}
 
     if model_data:
-        data['id'] = model_data.id
-        data['title'] = model_data.title
-        data['content'] = model_data.content
-        data['price'] = model_data.price
+        data = model_to_dict(model_data, fields=['id', 'title', 'price'])
+
     return JsonResponse(data)
 
 # def api_home(request, *args, **kwargs):
--- py_client/basic.py
+++ py_client/basic.py
@@ -9,5 +9,6 @@ get_response = requests.get(endpoint, params={"abc": 123}, json={"query":"Hello 
 
 print(get_response.json())
 
+
 # print(get_response.status_code)
 # print(get_response.text) # print raw text response
