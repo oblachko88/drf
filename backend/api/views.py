837b096dba0d3af12a29266c28aec1c5925bbaaa
oblako88 <88oblachko@gmail.com>
2023-12-11 19:35:49 +0200

    commit

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,23 +1,22 @@
-import json
-from django.http import JsonResponse, HttpResponse
 from django.forms.models import model_to_dict
-    # SERIALIZATION
-    # model instance (model_data)
-    # turn a Python dict 
-    # return JSON to my client
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+ 
 from products.models import Product
+from products.serializers import ProductSerializer
 
-
+@api_view(["POST"])
 def api_home(request, *args, **kwargs):
-    model_data = Product.objects.all().order_by("?").first()
-    # This order_by("?") means we take only one random object from all of them. 
-    data = {}
+    """
+    Django Rest API View
+    """
 
-    if model_data:
-        data = model_to_dict(model_data, fields=['id', 'title', 'price'])
-
-    return JsonResponse(data)
+    serializer = ProductSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        # instance = serializer.save()
+        print(serializer.data)
+        return Response(serializer.data)
+    return Response({"invalid": "not good data"}, status=400)
 
 # def api_home(request, *args, **kwargs):
 #     # request -> HttpRequest -> Django
--- py_client/basic.py
+++ py_client/basic.py
@@ -1,11 +1,11 @@
-import requests
+import requests, json
 
 # endpoint = "https://httpbin.org/status/200"
 # endpoint = "https://httpbin.org/anything"
 
 endpoint = "http://localhost:8000/api/"
 
-get_response = requests.get(endpoint, params={"abc": 123}, json={"query":"Hello world!"}) # HTTP Request
+get_response = requests.post(endpoint, params={"abc": 123}, json={"title": "Danila", "content":"Hello world!", "price": 20.00}) # HTTP Request
 
 print(get_response.json())
 
